fab8f18b635d19fdc1e2133d0a94e0014e54b9f2
Mingyao Liu <MingyaoLiu@users.noreply.github.com>
2020-05-10 12:00:42 -0400

    Update run.bat

--- run.bat
+++ run.bat
@@ -4,11 +4,9 @@ if exist "C:\ProgramData\Anaconda3\_conda.exe" (
     call echo "Anaconda Already Installed at C:\ProgramData\Anaconda3"
 ) else (
     call echo "Installing Anaconda at C:\ProgramData\Anaconda3"
-    if exist ".\Anaconda3-Windows-x86_64.exe" (
-    ) else (
-        call curl https://repo.anaconda.com/archive/Anaconda3-2020.02-Windows-x86_64.exe -o Anaconda3-Windows-x86_64.exe
-    )
+    call curl https://repo.anaconda.com/archive/Anaconda3-2020.02-Windows-x86_64.exe -o Anaconda3-Windows-x86_64.exe
     call start /wait "" Anaconda3-Windows-x86_64.exe /InstallationType=AllUsers /AddToPath=1 /RegisterPython=1 /S /D=C:\ProgramData\Anaconda3
+    call del Anaconda3-Windows-x86_64.exe
 )
 
 call "C:\ProgramData\Anaconda3\Scripts\activate.bat"
